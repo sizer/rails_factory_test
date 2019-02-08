fb5a5a61deedd42a14a8d5b053e5d8d2f24b7fa1
sizer <shinse.tanaka@gmail.com>
2019-02-08 19:59:23 +0900

    create relation between User <-- Participations --> Team

--- rails50/app/models/team.rb
+++ rails50/app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  has_many :participations
+  has_many :users, through: :participations
 end
--- rails50/app/models/user.rb
+++ rails50/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :participations
+  has_many :teams, through: :participations
 end
