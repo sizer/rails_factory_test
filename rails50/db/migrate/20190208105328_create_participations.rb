b2dbf1a6db7231b9dea5306b15564ab53d53cf72
sizer <shinse.tanaka@gmail.com>
2019-02-13 12:22:33 +0900

    create company

--- rails50/db/migrate/20190208105328_create_participations.rb
+++ rails50/db/migrate/20190208105328_create_participations.rb
@@ -1,6 +1,7 @@
 class CreateParticipations < ActiveRecord::Migration[5.0]
   def change
     create_table :participations do |t|
+      t.references :company, foreign_key: true
       t.references :user, foreign_key: true
       t.references :team, foreign_key: true
 
